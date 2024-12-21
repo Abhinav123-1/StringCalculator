ae04505a1ed1af1aa1a401357b1656b0bbc52a5a
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-12-21 19:37:01 +0530

    Test case for handling Negative Integers

--- src/test/java/com/project/stringcalculator/service/StringCalculatorTest.java
+++ src/test/java/com/project/stringcalculator/service/StringCalculatorTest.java
@@ -41,4 +41,11 @@ public class StringCalculatorTest {
         assertEquals(3, calculator.add("//;\n1;2"));
     }
 
+    @Test
+    void testNegativeNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> calculator.add("1,-2,3,-4"));
+        assertEquals("Negatives not allowed: -2 -4", exception.getMessage());
+    }
+
 }
