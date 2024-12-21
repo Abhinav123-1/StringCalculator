9fb26bb0ea626b22cedcceb09364dc672d5c8c24
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-12-21 16:26:42 +0530

    Test for Single Input should return itself

--- src/test/java/com/project/stringcalculator/service/StringCalculatorTest.java
+++ src/test/java/com/project/stringcalculator/service/StringCalculatorTest.java
@@ -11,4 +11,10 @@ public class StringCalculatorTest {
         assertEquals(0, calculator.add(""));
     }
 
+    @Test
+    void testSingleStringValueShouldGiveSameValue(){
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.add("1"));
+    }
+
 }
