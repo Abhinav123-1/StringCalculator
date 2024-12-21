4535007f6f51dca84d9023e4dc50c3762d9c0364
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-12-21 19:22:06 +0530

    Logic for handling input with custom Delimiters.

--- src/main/java/com/project/stringcalculator/service/StringCalculator.java
+++ src/main/java/com/project/stringcalculator/service/StringCalculator.java
@@ -6,7 +6,13 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] numbersArray = numbers.split("[,\n]");
+        String delimiter = "[,\n]";
+        if(numbers.startsWith("//")){
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numbers = numbers.substring(delimiterIndex + 1);
+        }
+        String[] numbersArray = numbers.split(delimiter);
         int sum = 0;
         for (String number : numbersArray) {
             sum += Integer.parseInt(number);
