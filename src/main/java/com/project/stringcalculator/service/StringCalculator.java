352cf96b914dad2df7cef21564bc9eba3bf2e432
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-12-21 21:16:15 +0530

    Logic for handling Negative Integers

--- src/main/java/com/project/stringcalculator/service/StringCalculator.java
+++ src/main/java/com/project/stringcalculator/service/StringCalculator.java
@@ -7,16 +7,28 @@ public class StringCalculator {
             return 0;
         }
         String delimiter = "[,\n]";
-        if(numbers.startsWith("//")){
+        if (numbers.startsWith("//")) {
             int delimiterIndex = numbers.indexOf("\n");
             delimiter = numbers.substring(2, delimiterIndex);
             numbers = numbers.substring(delimiterIndex + 1);
         }
         String[] numbersArray = numbers.split(delimiter);
         int sum = 0;
+        StringBuilder negatives = new StringBuilder();
+
         for (String number : numbersArray) {
-            sum += Integer.parseInt(number);
+            int num = Integer.parseInt(number);
+            if (num < 0) {
+                negatives.append(num).append(" ");
+            } else {
+                sum += num;
+            }
+        }
+
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException("Negatives not allowed: " + negatives.toString().trim());
         }
         return sum;
     }
+
 }
