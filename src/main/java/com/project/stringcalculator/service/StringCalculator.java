7202d76bae0b5a004a926f224f9270fd8e36a981
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-12-21 16:06:44 +0530

    Logic to return 0 for empty string input

---
+++ src/main/java/com/project/stringcalculator/service/StringCalculator.java
@@ -0,0 +1,11 @@
+package com.project.stringcalculator.service;
+
+public class StringCalculator {
+
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return -1;
+    }
+}
